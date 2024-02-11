aa574e8f9f5d7d3b56b740a2c982a174f539811f
kleenkanteen <antpalmolive@gmail.com>
2024-02-11 12:39:24 -0500

    basic landing page done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import type { AppProps } from 'next/app';
+import cspMiddleware from '../middlewares/csp';
 
 import { Inter } from "next/font/google";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,45 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+      <div className="flex flex-row justify-center items-center gap-12 px-4 py-4 m-0">
+        <img src="/logo.png" alt="Logo" width="150" height="150" />
+        <span className="text-5xl sm:text-[5rem] font-extrabold bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 inline-block text-transparent bg-clip-text">
+          skmAI
+        </span>
+      </div>
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[4rem]">
+          <span className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 inline-block text-transparent bg-clip-text">AI-Powered</span>
+          {' '}
+          Youtube Video Search
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+        <h2 className="text-3xl sm:text-[3rem] font-extrabold tracking-tight text-white leading-normal text-center">
+          Type your
+          {' '}
+          <span className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 inline-block text-transparent bg-clip-text">
+            thoughts
+          </span>
+          {' '}
+          and instantly get relevant timestamps
+        </h2>
+
+        <video width="960" height="720" controls preload="none">
+          <source src="/demo.mp4" type="video/mp4" />
+          <track
+            src="/demo.mp4"
+            kind="subtitles"
+            label="English"
+          />
+          Your browser does not support the video tag.
+        </video>
+
+        <Link
+          className="max-w-lg rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+          href="https://chromewebstore.google.com/detail/skmai-ai-powered-youtube/nkkklchgjghdppjfponpogcfgggchjef"
+          target="_blank"
+        >
+          <h3 className="text-2xl font-bold">Get it from the Chrome web store →</h3>
+        </Link>
       </div>
     </main>
   );
